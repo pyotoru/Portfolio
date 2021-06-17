2af041009ddc87699e7d727c1b7e6c4a310353f1
pyotoru <wawrzyniak.peter@gmail.com>
2021-06-18 00:14:12 +0900

    changed navbar

--- js-collapse.js
+++ js-collapse.js
@@ -12,3 +12,15 @@ for (i = 0; i < coll.length; i++) {
 		}
 	});
 }
+
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav-link");
+navToggle.addEventListener("click", () => {
+	document.body.classList.toggle("nav-open");
+});
+
+navLinks.forEach((link) => {
+	link.addEventListener("click", () => {
+		document.body.classList.remove("nav-open");
+	});
+});
