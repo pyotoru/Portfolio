558db5e60a15103539341f716c6287d457925c7c
Piotr Wawrzyniak <wawrzyniak.peter@gmail.com>
2021-11-23 13:06:22 +0900

    restoring code

---
+++ index.js
@@ -0,0 +1,85 @@
+//collapsible JS -currently not used
+
+var coll = document.getElementsByClassName("collapsibleJS");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav-link");
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
+});
+
+//html-css collapse -currently not used
+
+var coll = document.getElementsByClassName("collapsibleHTMLcss");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+//image load
+
+document.addEventListener("DOMContentLoaded", function () {
+  var lazyloadImages = document.querySelectorAll("img.lazy");
+  var lazyloadThrottleTimeout;
+
+  function lazyload() {
+    if (lazyloadThrottleTimeout) {
+      clearTimeout(lazyloadThrottleTimeout);
+    }
+
+    lazyloadThrottleTimeout = setTimeout(function () {
+      var scrollTop = window.pageYOffset;
+      lazyloadImages.forEach(function (img) {
+        if (img.offsetTop < window.innerHeight + scrollTop) {
+          img.src = img.dataset.src;
+          img.classList.remove("lazy");
+        }
+      });
+      if (lazyloadImages.length == 0) {
+        document.removeEventListener("scroll", lazyload);
+        window.removeEventListener("resize", lazyload);
+        window.removeEventListener("orientationChange", lazyload);
+      }
+    }, 20);
+  }
+
+  document.addEventListener("scroll", lazyload);
+  window.addEventListener("resize", lazyload);
+  window.addEventListener("orientationChange", lazyload);
+});
+
+//change colors on click
+
+$(document).ready(function () {
+  $("#change-colors-id").click(function () {
+    $("body").toggleClass("change-colors-action");
+  });
+});
